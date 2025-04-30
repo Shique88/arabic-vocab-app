6f6ebd173add38066723c036c5531798582a68c4
Shique88 <40884166+Shique88@users.noreply.github.com>
2025-04-30 20:39:32 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAH93WwphTZnCKhfmZv0EXz2ZC2LMJRfCU",
+  authDomain: "arabic-vocabulary-app.firebaseapp.com",
+  projectId: "arabic-vocabulary-app",
+  storageBucket: "arabic-vocabulary-app.firebasestorage.app",
+  messagingSenderId: "416598620126",
+  appId: "1:416598620126:web:ced53e36c34b9ed25bcf4f",
+  measurementId: "G-3YGS96ZS1F"
+};
